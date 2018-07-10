e61cad7acccc9b76cd531171ddb6d7551c5cb074
root <kieran.houtgraaf@gmail.com>
2018-07-10 09:53:14 +0200

    Improved the password system

--- install/install.sh
+++ install/install.sh
@@ -3,10 +3,11 @@
 cd $PWD
 
 sudo sed -i "s/password/$1/g" ./createdb.sql
-sudo sed -i "s/password/$1/g" ../sql/run.sh
+sudo sed -i "s/password/$1/g" ../sql/del.sh
+sudo sed -i "s/password/$1/g" ../php/funcs.php
 
 echo "Enter the root password for mysql"
 
 mysql -u root -p < ./createdb.sql
 
-sudo chmod 777 ../sql/run.sh
+sudo chmod 777 ../sql/del.sh
